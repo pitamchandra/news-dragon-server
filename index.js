67449f3af2cab8b21fa772b9c8ddf03997ba7604
pitamchandra <pitamchandra402@gmail.com>
2023-04-29 20:09:41 +0600

    added vercel

--- index.js
+++ index.js
@@ -4,15 +4,38 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors())
 const category = require('./data/category.json')
+const news = require('./data/news.json')
 
 app.get('/', (req, res) =>{
-    res.send('hello sdfsdf express')
+    res.send('my server is running')
 })
 
 app.get('/category', (req, res) =>{
     res.send(category)
 })
 
+app.get('/news', (req, res)=>{
+    res.send(news)
+})
+
+app.get('/news/:id' , (req, res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    if(id === 0 ){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
+    
+})
+
 app.listen(port, () =>{
-    console.log(`listing kkkdd port is : ${port}`);
+    console.log(`listing port is : ${port}`);
 })
